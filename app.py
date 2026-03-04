692804dbb37f6d47da0fd67900627f49a7587dee
shravani balu dhaybar <dhaybarshravani004@gmail.com>
2026-03-04 07:32:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from ai.basic_ai import analyze
 from security.basic_scan import scan
+from automation.auto_action import auto_action
 
 def main():
     target = "demo_target"
     issues = scan(target)
     risk = analyze(issues)
+    action = auto_action(risk)
+    print("Action:", action)
     print("Target:", target)
     print("Issues:", issues)
     print("Risk:", risk)
